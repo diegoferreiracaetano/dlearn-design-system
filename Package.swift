024cd9601da55edd7f4450ae66f71190549c463b
Anka <runner@sat12-dp150-9c43210e-1a58-420c-80e1-3aafbe6234c8-0AB8CA4212A8.local>
2025-10-17 12:41:21 +0000

    KMP SPM package release for 1.0.2

--- Package.swift
+++ Package.swift
@@ -1,17 +1,29 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/diegoferreiracaetano/dlearn-design-system/releases/assets/305457653.zip"
+let remoteKotlinChecksum = "8bc06add8535d6dc6b1152c58a90710e875c40fabcbffc00b701570d866930c1"
+let packageName = "designsystem"
+// END KMMBRIDGE BLOCK
+
 let package = Package(
-    name: "DesignSystem",
-    platforms: [.iOS(.v14)],
+    name: packageName,
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
-        .library(name: "DesignSystem", targets: ["DesignSystem"])
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
     ],
     targets: [
         .binaryTarget(
-            name: "DesignSystem",
-            url: "https://github.com/diegoferreiracaetano/dlearn-design-system/releases/download/1.0.1/DesignSystem.xcframework.zip",
-            checksum: "027a47ec35be32f2d0155f1c790e4154418ce79cdb04a18639e6b9908ffddaf3"
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
+        ,
     ]
 )
