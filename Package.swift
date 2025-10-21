1f4bdc438cfc2197f6312961477fab29fe598d55
Anka <runner@sat12-bq164-d3993c26-677e-48d1-9211-1987b40f8c33-E64AC03BE581.local>
2025-10-21 13:46:46 +0000

    KMP SPM package release for 1.0.1

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/diegoferreiracaetano/dlearn-design-system/releases/assets/306908501.zip"
+let remoteKotlinChecksum = "758ab1bbaaa6bf3194faee4f856225b2cb00099eb6f70a16a6077d3f5bf82758"
+let packageName = "DesignSystem"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
