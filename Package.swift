4b624455729b4db4f79dce7f0ef4a4c27a3127a1
Anka <runner@iad20-fj917-8a06ffef-1301-44ed-8095-4af680206597-B60E959009DC.local>
2025-10-26 18:35:30 +0000

    KMP SPM package release for 1.0.5

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ let packageName = "DesignSystem"
 let packageNameUI = "DesignSystemUI"
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/diegoferreiracaetano/dlearn-design-system/releases/assets/308932346.zip"
+let remoteKotlinChecksum = "d65dc2277dbff6936533c3300e15435d51a25b605cc55c7d02d03a9c3bdb4045"
+let packageName = "DesignSystem"
 // END KMMBRIDGE BLOCK
 
 let package = Package(
