ba28f3b5ab71b2c7d3874a1512ca3716c2baec5c
Anka <runner@Mac-1761533680337.local>
2025-10-27 03:25:11 +0000

    KMP SPM package release for 1.0.13

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let packageNameUI = "DesignSystemUI"
 let swiftSourcePath = "designsystem/src/iosMain/swift"
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/diegoferreiracaetano/dlearn-design-system/releases/assets/309080633.zip"
+let remoteKotlinChecksum = "019dfe315191bf688a4e54ab491b6070cd357f035092bbec0e807af23ab69ebd"
 let packageName = "DesignSystem"
-let remoteKotlinUrl = "remoteKotlinUrl"
-let remoteKotlinChecksum = "remoteKotlinChecksum"
 // END KMMBRIDGE BLOCK
 
 let binaryPath = "designsystem/build/XCFrameworks/release/\(packageName).xcframework"
